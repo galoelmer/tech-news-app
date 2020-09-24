94621403a36dc18821dd055a8846f0ff6591fa38
Elmer C. Galo <elmer.galo@outlook.com>
2020-09-23 23:29:28 -0400

    Add backend function to fetch news data from API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '../components/Card';
-import testData from '../data';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,26 +12,43 @@ const useStyles = makeStyles((theme) => ({
 
 export default function FullWidthGrid() {
   const classes = useStyles();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('/.netlify/functions/fetch-news')
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.data);
+      });
+  }, []);
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        {testData.articles.map((item) => {
-            if (!item.title || !item.description || !item.urlToImage) {
+        {data.length ? (
+          (
+          data.map((item) => {
+              if (!item.title || !item.description || !item.urlToImage) {
                 return null;
-            }
-          return (
-            <Grid key={item.title} item xs={12} sm={6} md={4} lg={3} xl={2}>
-              <Card
-                title={item.title}
-                description={item.description}
-                newsUrl={item.url}
-                imageUrl={item.urlToImage}
-                datePublished={item.publishedAt}
-              />
-            </Grid>
-          );
-        })}
+              }
+              return (
+                <Grid key={item.title} item xs={12} sm={6} md={4} lg={3} xl={2}>
+                  <Card
+                    title={item.title}
+                    description={item.description}
+                    newsUrl={item.url}
+                    imageUrl={item.urlToImage}
+                    datePublished={item.publishedAt}
+                  />
+                </Grid>
+              );
+            })
+        )
+        ) : (
+  (
+                  <div>No news available</div>
+        )
+        )}
       </Grid>
     </div>
   );

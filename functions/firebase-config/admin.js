00ef862658a88ea06ce6fd73524b06ef43a5112a
Elmer C. Galo <elmer.galo@outlook.com>
2020-09-30 21:44:02 -0400

    Update user signupt to store user data to cloud firebase

--- functions/firebase-config/admin.js
+++ functions/firebase-config/admin.js
@@ -1,6 +1,11 @@
 const admin = require('firebase-admin');
 
-admin.initializeApp();
+var serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://tech-news-app-4e549.firebaseio.com',
+});
 
 const db = admin.firestore();
 
--- functions/signup/signup.js
+++ functions/signup/signup.js
@@ -1,17 +1,54 @@
 const firebase = require('../firebase-config/config');
+const { db } = require('../firebase-config/admin');
 
-exports.handler = async (event) => {
+exports.handler = async (event, context, callback) => {
   if (event.httpMethod !== 'POST')
-    return { statusCode: 400, body: 'Must POST to this function' };
+    return {
+      statusCode: 400,
+      body: 'Must POST to this function',
+    };
 
-  const { email, password } = JSON.parse(event.body);
+    /**
+     * TODO: 
+     * - Add user input validation
+     */
+
+  const { name = 'Anonymous', email, password } = JSON.parse(event.body);
 
   try {
-    const data = await firebase
+    const newUser = await firebase
       .auth()
       .createUserWithEmailAndPassword(email, password);
-    return { statusCode: 201, body: JSON.stringify(data) };
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() };
+
+    const userId = newUser.user.uid;
+    const token = await newUser.user.getIdToken();
+
+    const userCredentials = {
+      name,
+      email,
+      createdAt: new Date().toISOString(),
+    };
+
+    await db.collection('users').doc(userId).set(userCredentials);
+
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        token,
+      }),
+    });
+  } catch (error) {
+    if (error.code === 'auth/email-already-in-use') {
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({ email: error.message }),
+      });
+    }
+    return callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({
+        general: 'Something went wrong, please try again',
+      }),
+    });
   }
 };

9f7602a76ad288d5ed2bc389eacd8fd9ccd4466b
Elmer C. Galo <elmer.galo@outlook.com>
2021-07-06 01:12:02 -0400

    style: change background colors on share social media icons

--- src/components/ShareLinks.js
+++ src/components/ShareLinks.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import IconButton from '@material-ui/core/IconButton';
-import ShareIcon from '@material-ui/icons/Share';
-import { withStyles } from '@material-ui/core/styles';
+import React from "react";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import IconButton from "@material-ui/core/IconButton";
+import ShareIcon from "@material-ui/icons/Share";
+import { withStyles } from "@material-ui/core/styles";
 
-import Tippy from '@tippyjs/react';
-import 'tippy.js/dist/tippy.css';
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
 
 import {
   EmailShareButton,
@@ -21,15 +21,15 @@ import {
   TumblrIcon,
   TwitterShareButton,
   TwitterIcon,
-} from 'react-share';
+} from "react-share";
 
 const StyledMenu = withStyles({
   paper: {
-    border: '1px solid #d3d4d5',
-    '& ul': {
-      display: 'flex',
-      flexWrap: 'wrap',
-      width: '205px',
+    border: "1px solid #d3d4d5",
+    "& ul": {
+      display: "flex",
+      flexWrap: "wrap",
+      width: "205px",
     },
   },
 })((props) => (
@@ -37,12 +37,12 @@ const StyledMenu = withStyles({
     elevation={2}
     getContentAnchorEl={null}
     anchorOrigin={{
-      vertical: 'center',
-      horizontal: 'right',
+      vertical: "center",
+      horizontal: "right",
     }}
     transformOrigin={{
-      vertical: 'center',
-      horizontal: 'left',
+      vertical: "center",
+      horizontal: "left",
     }}
     {...props}
   />
@@ -81,12 +81,16 @@ export default function SimpleMenu({ url, title, description }) {
       >
         <MenuItem onClick={handleClose}>
           <EmailShareButton url={url} subject={title} body={description}>
-            <EmailIcon size={36} round={true} />
+            <EmailIcon size={36} round={true} bgStyle={{ fill: "#00b300" }} />
           </EmailShareButton>
         </MenuItem>
         <MenuItem onClick={handleClose}>
           <FacebookShareButton url={url}>
-            <FacebookIcon size={36} round={true} />
+            <FacebookIcon
+              size={36}
+              round={true}
+              bgStyle={{ fill: "#1773EA" }}
+            />
           </FacebookShareButton>
         </MenuItem>
         <MenuItem onClick={handleClose}>
@@ -96,12 +100,12 @@ export default function SimpleMenu({ url, title, description }) {
         </MenuItem>
         <MenuItem onClick={handleClose}>
           <RedditShareButton url={url} title={title}>
-            <RedditIcon size={36} round={true} />
+            <RedditIcon size={36} round={true} bgStyle={{ fill: "#FF4500" }} />
           </RedditShareButton>
         </MenuItem>
         <MenuItem onClick={handleClose}>
           <TumblrShareButton url={url}>
-            <TumblrIcon size={36} round={true} />
+            <TumblrIcon size={36} round={true} bgStyle={{ fill: "#2F4155" }} />
           </TumblrShareButton>
         </MenuItem>
         <MenuItem onClick={handleClose}>

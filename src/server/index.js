85316ba7ca4322b5e5443bb36faaf739d3d7f76c
Elmer C. Galo <elmer.galo@outlook.com>
2021-08-05 04:57:22 -0400

    chore(miragejs): add apis to handle login, signup requests on dev mode

--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,48 @@
-import { createServer } from 'miragejs';
-import data from './data';
+import { createServer, Response } from "miragejs";
+import data from "./data";
 
-export function makeServer({ environment = 'test' } = {}) {
+export function makeServer({ environment = "test" } = {}) {
   let server = createServer({
     environment,
 
-    seeds(server){
-      server.db.loadData(data)
+    seeds(server) {
+      server.db.loadData(data);
     },
 
     routes() {
-      this.namespace = 'api';
-      this.timing = 2000;
+      this.namespace = "api";
+      this.timing = 1500;
 
-      this.get('/get-news-data', (schema) => {
-        return {data: schema.db.data}
+      this.get("/get-news-data", (schema) => {
+        return { data: schema.db.data };
+      });
+
+      this.post("/reset-password", () => {
+        throw new Error("Reset password not available on Dev mode");
+      });
+
+      this.post("/login", () => {
+        return new Response(
+          500,
+          { "Content-Type": "application/json" },
+          { general: "Login not available on Dev Mode" }
+        );
+      });
+
+      this.post("/signup", () => {
+        return new Response(
+          500,
+          { "Content-Type": "application/json" },
+          { general: "Signup not available on Dev Mode" }
+        );
+      });
+
+      this.post("/update-user-password", () => {
+        return new Response(
+          500,
+          { "Content-Type": "application/json" },
+          "Reset Password not available on Dev Mode"
+        );
       });
     },
   });
